e959fec5d0e3ac802b24bc6bf1474200564ae7f4
ricm167 <rikumasakamoto82@gmail.com>
2019-10-23 05:27:43 +0000

    fit

---
+++ Kadai2/index.js
@@ -0,0 +1,25 @@
+// 自分の名前は
+const firstName = 'Rikuma'
+//自分の苗字は
+const secondName = 'Sakamoto'
+//名前と苗字を組み合わせたら
+console.log(firstName+secondName)
+
+//1から10まで足してみる
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+
+//１から１０まで合計すると
+console.log(sum)
+
+//私は犬を飼っている
+const hasDog = true
+
+const hasCat = false
+
+
+console.log(hasDog||hasCat)
+
+console.log(hasDog&&hasCat)
+
+console.log(!hasDog||!hasCat)
+
