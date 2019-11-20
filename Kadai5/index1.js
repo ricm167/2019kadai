72790817f159fa6ea330ec2a7ddda3d97dc9fdce
ricm167 <rikumasakamoto82@gmail.com>
2019-11-20 04:52:59 +0000

    HTMLを追加

--- Kadai5/index1.js
+++ Kadai5/index1.js
@@ -2,7 +2,15 @@ const profile = {
     firstName: 'Rikuma',
     familyName: 'Sakamoto',
     age: 20,
-    hobby: 'go cycling',
+    hobby: 'dancing',
     major: 'archaeology',
+};
+
+const keks = Object.keys(profile);
+
+for (let i = 0; i < keys. length; i = i + 1 ){
+    const key = keys[i];
+    const value = profile[key];
+    const text = "私の" + key + "は" + value + "です。";
+    console.log(text);
 }
-for
